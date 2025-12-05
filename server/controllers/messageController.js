aa6c32d96dbd64825d89e56faa3e8fc45ded63a2
sunnymaurya719 <sunnymaurya1920@gmail.com>
2025-12-06 00:06:10 +0530

    Imagekit integration

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,65 +1,102 @@
 import openai from '../configs/openai.js';
 import Chat from '../models/Chat.js';
 import User from '../models/User.js';
+import axios from 'axios';
+import imagekit from '../configs/imagekit.js';
 
 
 //Text-based Ai Chat Message Controller
-export const textMessageController = async(req ,res) =>{
-    try{
+export const textMessageController = async (req, res) => {
+    try {
         const userId = req.user._id;
-        const {chatId,prompt} = req.body;
+        
+        if (req.user.credits < 1) {
+            return res.json({ success: false, message: "Not enough credits" });
+        }
+        const { chatId, prompt } = req.body;
 
-        const chat = await Chat.findOne({userId,_id:chatId});
+        const chat = await Chat.findOne({ userId, _id: chatId });
         chat.messages.push({
-            isImage:false,
-            role:'user',
-            content:prompt,
-            timestamp:Date.now()
+            isImage: false,
+            role: 'user',
+            content: prompt,
+            timestamp: Date.now()
         });
 
-        const {choices} = await openai.chat.completions.create({
-            model:'gemini-2.0-flash',
-            messages:[
+        const { choices } = await openai.chat.completions.create({
+            model: 'gemini-2.0-flash',
+            messages: [
                 {
-                    role:'user',
-                    content:prompt,
+                    role: 'user',
+                    content: prompt,
                 }
             ]
         });
 
-        const reply = {...choices[0].message,timestamp:Date.now(),isImage:false}
-        res.json({success:true,reply});
+        const reply = { ...choices[0].message, timestamp: Date.now(), isImage: false }
+        res.json({ success: true, reply });
         chat.messages.push(reply);
         await chat.save();
-        await User.updateOne({_id:userId},{$inc: {credits: -1}});
+        await User.updateOne({ _id: userId }, { $inc: { credits: -1 } });
 
-    }catch(error){
-        res.json({success:false,error:error.message});
+    } catch (error) {
+        res.json({ success: false, error: error.message });
     }
 }
 
 
 
 //Image Generation Message Controller
-export const imageMessageController = async(req,res) =>{
-    try{
+export const imageMessageController = async (req, res) => {
+    try {
         const userId = req.user._id;
-        if(req.user.credits < 2){
-            return res.json({success:false,message:"Not enough credits"});
+        if (req.user.credits < 2) {
+            return res.json({ success: false, message: "Not enough credits" });
         }
-        const {prompt,chatId,isPublished} = req.body;
+        const { prompt, chatId, isPublished } = req.body;
 
         //Find chat
-        const chat = await Chat.findOne({userId,_id:chatId});
+        const chat = await Chat.findOne({ userId, _id: chatId });
         //push user message to chat
         chat.messages.push({
-            role:'user',
-            content:prompt,
-            isImage:false,
-            timestamp:Date.now()
-        })
+            role: 'user',
+            content: prompt,
+            isImage: false,
+            timestamp: Date.now()
+        });
 
-    }catch(error){
-        res.json({success:false,error:error.message});
+        //Encode the prompt 
+        const encodedPrompt = encodeURIComponent(prompt);
+
+        //construct Imagekit AI generation URL
+        const generatedImageUrl = `${process.env.IMAGEKIT_URL_ENDPOINT}/ik-genimg-prompt-${encodedPrompt}/SaraGPT/${Date.now()}.png?tr=w-800,h-800`;
+
+        const AiImageResponse = await axios.get(generatedImageUrl, { responseType: 'arraybuffer' });
+
+        //Convert image data to base64
+        const base64Image = `data:image/png;base64,${Buffer.from(AiImageResponse.data, "binary").toString('base64')}`
+
+        //Upload to Imagekit Media Storage
+        const uploadResponse = await imagekit.upload({
+            file: base64Image,
+            fileName: `${Date.now()}.png`,
+            folder: 'SaraGPT'
+        })
+        const reply = {
+            role: 'assistant',
+            content: uploadResponse.url,
+            timestamp: Date.now(),
+            isImage: true,
+            isPublished
+        }
+        res.json({ success: true, reply });
+
+        chat.messages.push(reply);
+        await chat.save();
+        await User.updateOne({ _id: userId }, { $inc: { credits: -2 } });
+
+
+    } catch (error) {
+        res.json({ success: false, error: error.message });
     }
 }
