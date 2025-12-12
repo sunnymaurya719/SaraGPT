22fe9b767ff1a6721a470e593026a3d44889ccb9
sunnymaurya719 <sunnymaurya1920@gmail.com>
2025-12-12 21:55:40 +0530

    .

--- client/src/pages/Credits.jsx
+++ client/src/pages/Credits.jsx
@@ -1,14 +1,38 @@
 import React, { useEffect, useState } from 'react'
-import { dummyPlans } from '../assets/assets';
 import Loading from './Loading';
+import {useAppContext} from '../context/AppContext';
+import toast from 'react-hot-toast';
 
 const Credits = () => {
   const [plans,setPlans] = useState([]);
   const [loading,setLoading] = useState(true);
+  const {axios,token} = useAppContext();
 
   const fetchPlans = async() =>{
-    setPlans(dummyPlans)
-    setLoading(false)
+    try{
+      const res = await axios.get('/api/credit/plan',{headers:{Authorization:token}})
+      if(res.data.success){
+        setPlans(res.data.plans);
+      }else{
+        toast.error(res.data.message || 'Failed to fetch plans')
+      }
+    }catch(error){
+      toast.error(error.message);
+    }
+    setLoading(false);
+  }
+
+  const purchasePlan = async(planId) => {
+    try{
+      const res = await axios.post('/api/credit/purchase',{planId},{headers:{Authorization:token}});
+      if(res.data.success){
+        window.location.href = res.data.url;
+      }else{
+        toast.error(res.data.message);
+      }
+    }catch(error){
+      toast.error(error.message);
+    }
   }
 
   useEffect(()=>{
@@ -26,7 +50,7 @@ const Credits = () => {
             <div key={plan._id} className={`border border-gray-200 dark:border-purple-700 rounded-lg shadow hover:shadow-lg transition-shadow p-6 min-w-[300px] flex flex-col ${plan._id === "pro" ? "bg-purple-50 dark:bg-purple-900":"bg-white dark:bg-transparent"}`}>
               <div className='flex-1'>
                 <h3 className='text-xl font-semibold text-gray-900 dark:text-white mb-2'>{plan.name}</h3>
-                <p className='text-2xl font-bold text-purple-600 dark:text-purple-300'>${plan.price}<span className='text-base font-normal text-gray-600 dark:text-purple-200'>{' '}/ {plan.credits} credits</span></p>
+                <p className='text-2xl font-bold text-purple-600 dark:text-purple-300'>₹{plan.price}<span className='text-base font-normal text-gray-600 dark:text-purple-200'>{' '}/ {plan.credits} credits</span></p>
                 <ul className='list-disc list-inside text-sm text-gray-700 dark:text-purple-200 space-y-1'>
                   {
                     plan.features.map((feature,index)=>(
@@ -35,7 +59,7 @@ const Credits = () => {
                   }
                 </ul>
               </div>
-              <button className='mt-6 bg-purple-500 hover:bg-purple-700 active:bg-purple-800 text-white font-medium py-2 rounded transition-colors cursor-pointer'>Buy Now</button>
+              <button onClick={()=> toast.promise(purchasePlan(plan._id),{loading:'Processing...'})} className='mt-6 bg-purple-500 hover:bg-purple-700 active:bg-purple-800 text-white font-medium py-2 rounded transition-colors cursor-pointer'>Buy Now</button>
             </div>
           ))
         }
